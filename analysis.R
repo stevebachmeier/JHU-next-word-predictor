1c94c99263b951bc17bb55a18fb6f198517cd1dc
Steve Bachmeier <steve.d.bachmeier@gmail.com>
2018-09-27 15:42:23 -0700

    Fixed first prototype

--- analysis.R
+++ analysis.R
@@ -35,7 +35,7 @@ news_sample <- sample(x=news_raw, size=round(length(news_raw)*0.01),
 blogs_sample <- sample(x=blogs_raw, size=round(length(blogs_raw)*0.01), 
                                           replace=F)
 all_sample <- c(twitter_sample, news_sample, blogs_sample)
-# test_sample <- sample(c(twitter_sample, blogs_sample, news_sample), 10, replace=F)
+
 
 # ==============================================================================
 # CLEAN TEXT
@@ -95,8 +95,8 @@ all_freq20 <- data.frame(freq_terms(text.var=all_sample_clean, top=20, at.least=
 
 # ---- FREQUENCY PLOTS ----
 # Top, with and without stopwords
-dev.new()
 plot.new()
+dev.new()
 par(mfrow=c(1,2), mar=c(6.2,6,5.5,0))
 
 barplot(height=all_freq20_wStopwords$FREQ, names.arg=all_freq20_wStopwords$WORD, 
@@ -107,7 +107,6 @@ barplot(height=all_freq20$FREQ, names.arg=all_freq20$WORD, horiz=F,
 mtext("Top 20 unigrams", side=1, line=-2, outer=T, cex=1)
 mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
 
-dev.off()
 # Top (all sources), with stopwords
 # dev.new()
 # par(mfrow=c(2,2), mar=c(6.2,6,5.5,0))
@@ -139,32 +138,31 @@ dev.off()
 # mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
 
 # ---- TOKENIZE ----
-# test_tokens <- tokens(x=test_sample_clean)
-twitter_tokens <- tokens(x=twitter_sample_clean)
-news_tokens <- tokens(x=news_sample_clean)
-blogs_tokens <- tokens(x=blogs_sample_clean)
+# twitter_tokens <- tokens(x=twitter_sample_clean)
+# news_tokens <- tokens(x=news_sample_clean)
+# blogs_tokens <- tokens(x=blogs_sample_clean)
 all_tokens <- tokens(x=all_sample_clean)
 
 # ---- N-GRAMS ----
 
 # Twitter 
-twitter_2grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=2, verbose=F), n=Inf))
-twitter_2grams$names <- rownames(twitter_2grams)
-colnames(twitter_2grams)[1] <- "ngrams2"
-rownames(twitter_2grams) <- NULL
-twitter_2grams <- select(twitter_2grams, names, ngrams2)
-
-twitter_3grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=3, verbose=F), n=Inf))
-twitter_3grams$names <- rownames(twitter_3grams)
-colnames(twitter_3grams)[1] <- "ngrams3"
-rownames(twitter_3grams) <- NULL
-twitter_3grams <- select(twitter_3grams, names, ngrams3)
-
-twitter_4grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=4, verbose=F), n=Inf))
-twitter_4grams$names <- rownames(twitter_4grams)
-colnames(twitter_4grams)[1] <- "ngrams4"
-rownames(twitter_4grams) <- NULL
-twitter_4grams <- select(twitter_4grams, names, ngrams4)
+# twitter_2grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=2, verbose=F), n=Inf))
+# twitter_2grams$names <- rownames(twitter_2grams)
+# colnames(twitter_2grams)[1] <- "ngrams2"
+# rownames(twitter_2grams) <- NULL
+# twitter_2grams <- select(twitter_2grams, names, ngrams2)
+# 
+# twitter_3grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=3, verbose=F), n=Inf))
+# twitter_3grams$names <- rownames(twitter_3grams)
+# colnames(twitter_3grams)[1] <- "ngrams3"
+# rownames(twitter_3grams) <- NULL
+# twitter_3grams <- select(twitter_3grams, names, ngrams3)
+# 
+# twitter_4grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=4, verbose=F), n=Inf))
+# twitter_4grams$names <- rownames(twitter_4grams)
+# colnames(twitter_4grams)[1] <- "ngrams4"
+# rownames(twitter_4grams) <- NULL
+# twitter_4grams <- select(twitter_4grams, names, ngrams4)
 
 # twitter_5grams <- data.frame(topfeatures(dfm(twitter_tokens, ngrams=5, verbose=F), n=Inf))
 # twitter_5grams$names <- rownames(twitter_5grams)
@@ -173,23 +171,23 @@ twitter_4grams <- select(twitter_4grams, names, ngrams4)
 # twitter_5grams <- select(twitter_5grams, names, ngrams5)
 
 # News 
-news_2grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=2, verbose=F), n=Inf))
-news_2grams$names <- rownames(news_2grams)
-colnames(news_2grams)[1] <- "ngrams2"
-rownames(news_2grams) <- NULL
-news_2grams <- select(news_2grams, names, ngrams2)
-
-news_3grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=3, verbose=F), n=Inf))
-news_3grams$names <- rownames(news_3grams)
-colnames(news_3grams)[1] <- "ngrams3"
-rownames(news_3grams) <- NULL
-news_3grams <- select(news_3grams, names, ngrams3)
-
-news_4grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=4, verbose=F), n=Inf))
-news_4grams$names <- rownames(news_4grams)
-colnames(news_4grams)[1] <- "ngrams4"
-rownames(news_4grams) <- NULL
-news_4grams <- select(news_4grams, names, ngrams4)
+# news_2grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=2, verbose=F), n=Inf))
+# news_2grams$names <- rownames(news_2grams)
+# colnames(news_2grams)[1] <- "ngrams2"
+# rownames(news_2grams) <- NULL
+# news_2grams <- select(news_2grams, names, ngrams2)
+# 
+# news_3grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=3, verbose=F), n=Inf))
+# news_3grams$names <- rownames(news_3grams)
+# colnames(news_3grams)[1] <- "ngrams3"
+# rownames(news_3grams) <- NULL
+# news_3grams <- select(news_3grams, names, ngrams3)
+# 
+# news_4grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=4, verbose=F), n=Inf))
+# news_4grams$names <- rownames(news_4grams)
+# colnames(news_4grams)[1] <- "ngrams4"
+# rownames(news_4grams) <- NULL
+# news_4grams <- select(news_4grams, names, ngrams4)
 
 # news_5grams <- data.frame(topfeatures(dfm(news_tokens, ngrams=5, verbose=F), n=Inf))
 # news_5grams$names <- rownames(news_5grams)
@@ -198,23 +196,23 @@ news_4grams <- select(news_4grams, names, ngrams4)
 # news_5grams <- select(news_5grams, names, ngrams5)
 
 # Blogs 
-blogs_2grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=2, verbose=F), n=Inf))
-blogs_2grams$names <- rownames(blogs_2grams)
-colnames(blogs_2grams)[1] <- "ngrams2"
-rownames(blogs_2grams) <- NULL
-blogs_2grams <- select(blogs_2grams, names, ngrams2)
-
-blogs_3grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=3, verbose=F), n=Inf))
-blogs_3grams$names <- rownames(blogs_3grams)
-colnames(blogs_3grams)[1] <- "ngrams3"
-rownames(blogs_3grams) <- NULL
-blogs_3grams <- select(blogs_3grams, names, ngrams3)
-
-blogs_4grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=4, verbose=F), n=Inf))
-blogs_4grams$names <- rownames(blogs_4grams)
-colnames(blogs_4grams)[1] <- "ngrams4"
-rownames(blogs_4grams) <- NULL
-blogs_4grams <- select(blogs_4grams, names, ngrams4)
+# blogs_2grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=2, verbose=F), n=Inf))
+# blogs_2grams$names <- rownames(blogs_2grams)
+# colnames(blogs_2grams)[1] <- "ngrams2"
+# rownames(blogs_2grams) <- NULL
+# blogs_2grams <- select(blogs_2grams, names, ngrams2)
+# 
+# blogs_3grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=3, verbose=F), n=Inf))
+# blogs_3grams$names <- rownames(blogs_3grams)
+# colnames(blogs_3grams)[1] <- "ngrams3"
+# rownames(blogs_3grams) <- NULL
+# blogs_3grams <- select(blogs_3grams, names, ngrams3)
+# 
+# blogs_4grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=4, verbose=F), n=Inf))
+# blogs_4grams$names <- rownames(blogs_4grams)
+# colnames(blogs_4grams)[1] <- "ngrams4"
+# rownames(blogs_4grams) <- NULL
+# blogs_4grams <- select(blogs_4grams, names, ngrams4)
 
 # blogs_5grams <- data.frame(topfeatures(dfm(blogs_tokens, ngrams=5, verbose=F), n=Inf))
 # blogs_5grams$names <- rownames(blogs_5grams)
@@ -250,49 +248,74 @@ all_4grams <- select(all_4grams, names, ngrams4)
 # ---- N-GRAM PLOTS ----
 # 2-gram
 dev.new()
-par(mfrow=c(2,2), mar=c(5,4,5,0))
-barplot(height=twitter_2grams$ngrams2[1:20], names.arg=twitter_2grams$names[1:20], 
-        horiz=F, las=2, main="Twitter")
-barplot(height=news_2grams$ngrams2[1:20], names.arg=news_2grams$names[1:20], 
-        horiz=F, las=2, main="News")
-barplot(height=blogs_2grams$ngrams2[1:20], names.arg=blogs_2grams$names[1:20], 
-        horiz=F, las=2, main="Blogs")
+par(mfrow=c(1,1), mar=c(4,4,4,0))
 barplot(height=all_2grams$ngrams2[1:20], names.arg=all_2grams$names[1:20], 
-        horiz=F, las=2, main="Combined")
+        horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
 mtext("Top 20 2-grams", side=3, line=-2, outer=T, cex=1)
-mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
+
+# dev.new()
+# par(mfrow=c(2,2), mar=c(5,4,5,0))
+# barplot(height=twitter_2grams$ngrams2[1:20], names.arg=twitter_2grams$names[1:20], 
+#         horiz=F, las=2, main="Twitter")
+# barplot(height=news_2grams$ngrams2[1:20], names.arg=news_2grams$names[1:20], 
+#         horiz=F, las=2, main="News")
+# barplot(height=blogs_2grams$ngrams2[1:20], names.arg=blogs_2grams$names[1:20], 
+#         horiz=F, las=2, main="Blogs")
+# barplot(height=all_2grams$ngrams2[1:20], names.arg=all_2grams$names[1:20], 
+#         horiz=F, las=2, main="Combined")
+# mtext("Top 20 2-grams", side=3, line=-2, outer=T, cex=1)
+# mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
 
 # 3-grams
 dev.new()
-par(mfrow=c(2,2), mar=c(8,4,5,0))
-barplot(height=twitter_3grams$ngrams3[1:20], names.arg=twitter_3grams$names[1:20], 
-        horiz=F, las=2, main="Twitter")
-barplot(height=news_3grams$ngrams3[1:20], names.arg=news_3grams$names[1:20], 
-        horiz=F, las=2, main="News")
-barplot(height=blogs_3grams$ngrams3[1:20], names.arg=blogs_3grams$names[1:20], 
-        horiz=F, las=2, main="Blogs")
+par(mfrow=c(1,1), mar=c(6,4,4,0))
 barplot(height=all_3grams$ngrams3[1:20], names.arg=all_3grams$names[1:20], 
-        horiz=F, las=2, main="Combined")
+        horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
 mtext("Top 20 3-grams", side=3, line=-2, outer=T, cex=1)
-mtext("3-grams", side=1, line=-2, outer=T, cex=0.8)
-mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
+
+# dev.new()
+# par(mfrow=c(2,2), mar=c(8,4,5,0))
+# barplot(height=twitter_3grams$ngrams3[1:20], names.arg=twitter_3grams$names[1:20], 
+#         horiz=F, las=2, main="Twitter")
+# barplot(height=news_3grams$ngrams3[1:20], names.arg=news_3grams$names[1:20], 
+#         horiz=F, las=2, main="News")
+# barplot(height=blogs_3grams$ngrams3[1:20], names.arg=blogs_3grams$names[1:20], 
+#         horiz=F, las=2, main="Blogs")
+# barplot(height=all_3grams$ngrams3[1:20], names.arg=all_3grams$names[1:20], 
+#         horiz=F, las=2, main="Combined")
+# mtext("Top 20 3-grams", side=3, line=-2, outer=T, cex=1)
+# mtext("3-grams", side=1, line=-2, outer=T, cex=0.8)
+# mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
 
 # 4-grams
 dev.new()
-par(mfrow=c(2,2), mar=c(11,4,5,0))
-barplot(height=twitter_4grams$ngrams4[1:20], names.arg=twitter_4grams$names[1:20], 
-        horiz=F, las=2, main="Twitter")
-barplot(height=news_4grams$ngrams4[1:20], names.arg=news_4grams$names[1:20], 
-        horiz=F, las=2, main="News")
-barplot(height=blogs_4grams$ngrams4[1:20], names.arg=blogs_4grams$names[1:20], 
-        horiz=F, las=2, main="Blogs")
+par(mfrow=c(1,1), mar=c(8,4,4,0))
 barplot(height=all_4grams$ngrams4[1:20], names.arg=all_4grams$names[1:20], 
-        horiz=F, las=2, main="Combined")
+        horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
 mtext("Top 20 4-grams", side=3, line=-2, outer=T, cex=1)
-mtext("4-grams", side=1, line=-2, outer=T, cex=0.8)
-mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
+
+# 
+# dev.new()
+# par(mfrow=c(2,2), mar=c(11,4,5,0))
+# barplot(height=twitter_4grams$ngrams4[1:20], names.arg=twitter_4grams$names[1:20], 
+#         horiz=F, las=2, main="Twitter")
+# barplot(height=news_4grams$ngrams4[1:20], names.arg=news_4grams$names[1:20], 
+#         horiz=F, las=2, main="News")
+# barplot(height=blogs_4grams$ngrams4[1:20], names.arg=blogs_4grams$names[1:20], 
+#         horiz=F, las=2, main="Blogs")
+# barplot(height=all_4grams$ngrams4[1:20], names.arg=all_4grams$names[1:20], 
+#         horiz=F, las=2, main="Combined")
+# mtext("Top 20 4-grams", side=3, line=-2, outer=T, cex=1)
+# mtext("4-grams", side=1, line=-2, outer=T, cex=0.8)
+# mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
 
 # 5-grams
+# dev.new()
+# par(mfrow=c(1,1), mar=c(10,4,4,0))
+# barplot(height=all_5grams$ngrams5[1:20], names.arg=all_5grams$names[1:20], 
+#         horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
+# mtext("Top 20 5-grams", side=3, line=-2, outer=T, cex=1)
+
 # dev.new()
 # par(mfrow=c(2,2), mar=c(14,4,5,0))
 # barplot(height=twitter_5grams$ngrams5[1:20], names.arg=twitter_5grams$names[1:20], 
@@ -322,8 +345,8 @@ dev.off()
 # "I have a", 3grams for "have a", and 2grams for "a".
 # ==============================================================================
 
-nlpPredictor <- function(input) {
-  input <- input
+nlpPredictor <- function(x) {
+  input <- x
   input_tokens <- cleanWords(input) %>% strsplit(" ")
   search_tokens <- tail(input_tokens[[1]], 3)
   search_string <- paste0(paste(search_tokens, collapse="_"), "_")
@@ -336,6 +359,7 @@ nlpPredictor <- function(input) {
     ngrams_matched <- all_2grams[grep(search_string, all_2grams$names)[1:3],]
   }
   
+  pred <- matrix(data=NA, nrow=1, ncol=3)
   for (i in 1:3) {
     pred[i] <- strsplit(ngrams_matched$names, "_")[[i]][length(strsplit(ngrams_matched$names, "_")[[i]])]
   }

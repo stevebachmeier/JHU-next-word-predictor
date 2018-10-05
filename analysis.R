f25ec47437b80ecc99b80b3f92027912a414c519
Steve Bachmeier <steve.d.bachmeier@gmail.com>
2018-10-04 17:39:16 -0700

    Made function

--- analysis.R
+++ analysis.R
@@ -97,42 +97,42 @@ all_4grams <- data.table(data.frame(topfeatures(
   dfm(all_tokens, ngrams=4, verbose=F), n=Inf)), keep.rownames=T)
 colnames(all_4grams) <- c("ngram","freq")
 
-# ---- FREQUENCY PLOTS ----
-# 1-gram
-plot.new()
-dev.new()
-par(mfrow=c(1,2), mar=c(6.2,6,5.5,0))
-
-barplot(height=all_1grams$freq[1:20], names.arg=all_1grams$ngram[1:20], horiz=F, 
-        las=2, main="With stopwords", cex.names=0.8, cex.axis=0.8, cex.main=0.8)
-
-barplot(height=all_1grams_noStopwords$freq[1:20], 
-        names.arg=all_1grams_noStopwords$ngram[1:20], horiz=F, las=2, 
-        main="No stopwords", cex.names = 0.8, cex.axis=0.8, cex.main=0.8)
-
-mtext("Top 20 unigrams", side=1, line=-2, outer=T, cex=1)
-mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
-
-# 2-gram
-dev.new()
-par(mfrow=c(1,1), mar=c(4,4,4,0))
-barplot(height=all_2grams$freq[1:20], names.arg=all_2grams$ngram[1:20], 
-        horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
-mtext("Top 20 2-grams", side=3, line=-2, outer=T, cex=1)
-
-# 3-grams
-dev.new()
-par(mfrow=c(1,1), mar=c(6,4,4,0))
-barplot(height=all_3grams$freq[1:20], names.arg=all_3grams$ngram[1:20], 
-        horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
-mtext("Top 20 3-grams", side=3, line=-2, outer=T, cex=1)
-
-# 4-grams
-dev.new()
-par(mfrow=c(1,1), mar=c(8,4,4,0))
-barplot(height=all_4grams$freq[1:20], names.arg=all_4grams$ngram[1:20], 
-        horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
-mtext("Top 20 4-grams", side=3, line=-2, outer=T, cex=1)
+# # ---- FREQUENCY PLOTS ----
+# # 1-gram
+# plot.new()
+# dev.new()
+# par(mfrow=c(1,2), mar=c(6.2,6,5.5,0))
+# 
+# barplot(height=all_1grams$freq[1:20], names.arg=all_1grams$ngram[1:20], horiz=F, 
+#         las=2, main="With stopwords", cex.names=0.8, cex.axis=0.8, cex.main=0.8)
+# 
+# barplot(height=all_1grams_noStopwords$freq[1:20], 
+#         names.arg=all_1grams_noStopwords$ngram[1:20], horiz=F, las=2, 
+#         main="No stopwords", cex.names = 0.8, cex.axis=0.8, cex.main=0.8)
+# 
+# mtext("Top 20 unigrams", side=1, line=-2, outer=T, cex=1)
+# mtext("Frequency", side=2, line=-2, outer=T, cex=0.8)
+# 
+# # 2-gram
+# dev.new()
+# par(mfrow=c(1,1), mar=c(4,4,4,0))
+# barplot(height=all_2grams$freq[1:20], names.arg=all_2grams$ngram[1:20], 
+#         horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
+# mtext("Top 20 2-grams", side=3, line=-2, outer=T, cex=1)
+# 
+# # 3-grams
+# dev.new()
+# par(mfrow=c(1,1), mar=c(6,4,4,0))
+# barplot(height=all_3grams$freq[1:20], names.arg=all_3grams$ngram[1:20], 
+#         horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
+# mtext("Top 20 3-grams", side=3, line=-2, outer=T, cex=1)
+# 
+# # 4-grams
+# dev.new()
+# par(mfrow=c(1,1), mar=c(8,4,4,0))
+# barplot(height=all_4grams$freq[1:20], names.arg=all_4grams$ngram[1:20], 
+#         horiz=F, las=2, ylab="Frequency", cex.axis=0.8, cex.names=0.8)
+# mtext("Top 20 4-grams", side=3, line=-2, outer=T, cex=1)
 
 # ==============================================================================
 # ANALYSIS
@@ -140,95 +140,96 @@ mtext("Top 20 4-grams", side=3, line=-2, outer=T, cex=1)
 # 
 
 # ---- STUPID BACKOFF ----
-alpha <- 0.4 # Stupid backoff factor
-input <- " What should I "
-
-ngrams_matched <- data.table(
-  ngram=character(), pred=character(), freq=numeric(), S=numeric())
-ngrams_matched_bo1 <- data.table(
-  ngram=character(), pred=character(), freq=numeric(), S=numeric())
-ngrams_matched_bo2 <- data.table(
-  ngram=character(), pred=character(), freq=numeric(), S=numeric())
-ngrams_matched_bo3 <- data.table(
-  ngram=character(), pred=character(), freq=numeric(), S=numeric())
-
-preds <- data.table(pred=all_1grams$ngram, S=0)
-preds_bo1 <- data.table(pred=all_1grams$ngram, S=0)
-preds_bo2 <- data.table(pred=all_1grams$ngram, S=0)
-preds_bo3 <- data.table(pred=all_1grams$ngram, S=0)
-
-# Initial search
-input_tokens <- cleanWords(input) %>% strsplit(" ")
-search_tokens <- tail(input_tokens[[1]], 3)
-search_string <- paste0("^", paste(search_tokens, collapse="_"), "_")
-n <- length(search_tokens)
-
-if (n == 3) {
-  ngrams_matched <- all_4grams[grep(search_string, all_4grams$ngram),]
-} else if (n == 2) {
-  ngrams_matched <- all_3grams[grep(search_string, all_3grams$ngram),]
-} else if (n == 1) {
-  ngrams_matched <- all_2grams[grep(search_string, all_2grams$ngram),]
-} else {
-  ngrams_matched <- all_1grams
+nlpPredictor <- function(input) {
+  alpha <- 0.4 # Stupid backoff factor
+  
+  ngrams_matched <- data.table(
+    ngram=character(), pred=character(), freq=numeric(), S=numeric())
+  ngrams_matched_bo1 <- data.table(
+    ngram=character(), pred=character(), freq=numeric(), S=numeric())
+  ngrams_matched_bo2 <- data.table(
+    ngram=character(), pred=character(), freq=numeric(), S=numeric())
+  ngrams_matched_bo3 <- data.table(
+    ngram=character(), pred=character(), freq=numeric(), S=numeric())
+  
+  preds <- data.table(pred=all_1grams$ngram, S=0)
+  preds_bo1 <- data.table(pred=all_1grams$ngram, S=0)
+  preds_bo2 <- data.table(pred=all_1grams$ngram, S=0)
+  preds_bo3 <- data.table(pred=all_1grams$ngram, S=0)
+  
+  # Initial search
+  input_tokens <- cleanWords(input) %>% strsplit(" ")
+  search_tokens <- tail(input_tokens[[1]], 3)
+  search_string <- paste0("^", paste(search_tokens, collapse="_"), "_")
+  n <- length(search_tokens)
+  
+  if (n == 3) {
+    ngrams_matched <- all_4grams[grep(search_string, all_4grams$ngram),]
+  } else if (n == 2) {
+    ngrams_matched <- all_3grams[grep(search_string, all_3grams$ngram),]
+  } else if (n == 1) {
+    ngrams_matched <- all_2grams[grep(search_string, all_2grams$ngram),]
+  } else {
+    ngrams_matched <- all_1grams
+  }
+  
+  ngrams_matched$pred <- str_remove(ngrams_matched$ngram, pattern=search_string)
+  ngrams_matched$S <- ngrams_matched$freq / sum(ngrams_matched$freq)
+  preds[preds$pred %in% ngrams_matched$pred,]$S <- ngrams_matched$S
+  
+  # Stupid backoff #1
+  search_tokens_bo1 <- search_tokens[-1]
+  search_string_bo1 <- paste0("^", paste(search_tokens_bo1, collapse="_"), "_")
+  n_bo1 <- length(search_tokens_bo1)
+  
+  if ((n_bo1 != n) & (n_bo1 == 2)) {
+    ngrams_matched_bo1 <- all_3grams[grep(search_string_bo1, all_3grams$ngram),]
+  } else if ((n_bo1 != n) & (n_bo1 == 1)) {
+    ngrams_matched_bo1 <- all_2grams[grep(search_string_bo1, all_2grams$ngram),]
+  } else if (n_bo1 != n) {
+    ngrams_matched_bo1 <- all_1grams
+  }
+  
+  ngrams_matched_bo1$pred <- str_remove(ngrams_matched_bo1$ngram, pattern=search_string_bo1)
+  ngrams_matched_bo1$S <- alpha^1 * (ngrams_matched_bo1$freq / sum(ngrams_matched_bo1$freq))
+  preds_bo1[preds_bo1$pred %in% ngrams_matched_bo1$pred,]$S <- ngrams_matched_bo1$S
+  
+  # Stupid backoff #2
+  search_tokens_bo2 <- search_tokens_bo1[-1]
+  search_string_bo2 <- paste0("^", paste(search_tokens_bo2, collapse="_"), "_")
+  n_bo2 <- length(search_tokens_bo2)
+  
+  if ((n_bo2 != n_bo1) & (n_bo2 == 1)) {
+    ngrams_matched_bo2 <- all_2grams[grep(search_string_bo2, all_2grams$ngram),]
+  } else if (n_bo2 != n_bo1) {
+    ngrams_matched_bo2 <- all_1grams
+  }
+  
+  ngrams_matched_bo2$pred <- str_remove(ngrams_matched_bo2$ngram, pattern=search_string_bo2)
+  ngrams_matched_bo2$S <- alpha^2 * (ngrams_matched_bo2$freq / sum(ngrams_matched_bo2$freq))
+  preds_bo2[preds_bo2$pred %in% ngrams_matched_bo2$pred,]$S <- ngrams_matched_bo2$S
+  
+  # Stupid backoff #3
+  search_tokens_bo3 <- search_tokens_bo2[-1]
+  search_string_bo3 <- paste0("^", paste(search_tokens_bo3, collapse="_"), "_")
+  n_bo3 <- length(search_tokens_bo3)
+  
+  if (n_bo3 != n_bo2) {
+    ngrams_matched_bo3 <- all_1grams
+  }
+  
+  ngrams_matched_bo3$pred <- str_remove(ngrams_matched_bo3$ngram, pattern=search_string_bo3)
+  ngrams_matched_bo3$S <- alpha^3 * (ngrams_matched_bo3$freq / sum(ngrams_matched_bo3$freq))
+  preds_bo3[preds_bo3$pred %in% ngrams_matched_bo3$pred,]$S <- ngrams_matched_bo3$S
+  
+  # Combine predictions
+  predictions <- data.table(word=preds$pred, S_0=preds$S, S_bo1=preds_bo1$S, 
+                            S_bo2=preds_bo2$S, S_bo3=preds_bo3$S,
+                            S=(preds$S + preds_bo1$S + preds_bo2$S + preds_bo3$S))
+  predictions <- predictions[order(-S)]
+  
+  cat(paste(input, ":", "\n", 
+            "  [1] ", predictions[1]$word, " ( S =", round(predictions[1]$S, 2), ")", "\n", 
+            "  [2] ", predictions[2]$word, " ( S =", round(predictions[2]$S, 2), ")", "\n",
+            "  [3] ", predictions[3]$word, " ( S =", round(predictions[3]$S, 2), ")"))
 }
-
-ngrams_matched$pred <- str_remove(ngrams_matched$ngram, pattern=search_string)
-ngrams_matched$S <- ngrams_matched$freq / sum(ngrams_matched$freq)
-preds[preds$pred %in% ngrams_matched$pred,]$S <- ngrams_matched$S
-
-# Stupid backoff #1
-search_tokens_bo1 <- search_tokens[-1]
-search_string_bo1 <- paste0("^", paste(search_tokens_bo1, collapse="_"), "_")
-n_bo1 <- length(search_tokens_bo1)
-
-if ((n_bo1 != n) & (n_bo1 == 2)) {
-  ngrams_matched_bo1 <- all_3grams[grep(search_string_bo1, all_3grams$ngram),]
-} else if ((n_bo1 != n) & (n_bo1 == 1)) {
-  ngrams_matched_bo1 <- all_2grams[grep(search_string_bo1, all_2grams$ngram),]
-} else if (n_bo1 != n) {
-  ngrams_matched_bo1 <- all_1grams
-}
-
-ngrams_matched_bo1$pred <- str_remove(ngrams_matched_bo1$ngram, pattern=search_string_bo1)
-ngrams_matched_bo1$S <- alpha^1 * (ngrams_matched_bo1$freq / sum(ngrams_matched_bo1$freq))
-preds_bo1[preds_bo1$pred %in% ngrams_matched_bo1$pred,]$S <- ngrams_matched_bo1$S
-
-# Stupid backoff #2
-search_tokens_bo2 <- search_tokens_bo1[-1]
-search_string_bo2 <- paste0("^", paste(search_tokens_bo2, collapse="_"), "_")
-n_bo2 <- length(search_tokens_bo2)
-
-if ((n_bo2 != n_bo1) & (n_bo2 == 1)) {
-  ngrams_matched_bo2 <- all_2grams[grep(search_string_bo2, all_2grams$ngram),]
-} else if (n_bo2 != n_bo1) {
-  ngrams_matched_bo2 <- all_1grams
-}
-
-ngrams_matched_bo2$pred <- str_remove(ngrams_matched_bo2$ngram, pattern=search_string_bo2)
-ngrams_matched_bo2$S <- alpha^2 * (ngrams_matched_bo2$freq / sum(ngrams_matched_bo2$freq))
-preds_bo2[preds_bo2$pred %in% ngrams_matched_bo2$pred,]$S <- ngrams_matched_bo2$S
-
-# Stupid backoff #3
-search_tokens_bo3 <- search_tokens_bo2[-1]
-search_string_bo3 <- paste0("^", paste(search_tokens_bo3, collapse="_"), "_")
-n_bo3 <- length(search_tokens_bo3)
-
-if (n_bo3 != n_bo2) {
-  ngrams_matched_bo3 <- all_1grams
-}
-
-ngrams_matched_bo3$pred <- str_remove(ngrams_matched_bo3$ngram, pattern=search_string_bo3)
-ngrams_matched_bo3$S <- alpha^3 * (ngrams_matched_bo3$freq / sum(ngrams_matched_bo3$freq))
-preds_bo3[preds_bo3$pred %in% ngrams_matched_bo3$pred,]$S <- ngrams_matched_bo3$S
-
-# Combine predictions
-predictions <- data.table(word=preds$pred, S_0=preds$S, S_bo1=preds_bo1$S, 
-                          S_bo2=preds_bo2$S, S_bo3=preds_bo3$S,
-                          S=(preds$S + preds_bo1$S + preds_bo2$S + preds_bo3$S))
-predictions <- predictions[order(-S)]
-
-cat(paste(input, ":", "\n", 
-          "  [1] ", predictions[1]$word, " (S=", round(predictions[1]$S, 2), ")", "\n", 
-          "  [2] ", predictions[2]$word, " (S=", round(predictions[2]$S, 2), ")", "\n",
-          "  [3] ", predictions[3]$word, " (S=", round(predictions[3]$S, 2), ")"))
